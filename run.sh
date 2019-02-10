74954fe57cc9cc58f0a545bdbdf8bf50f746ecf8
Alberto Lerdo <alb@intentionet.com>
2019-02-09 18:56:34 -0800

    add other files

---
+++ run.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+export PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+[ -n "$PROJECT_DIR" ]
+. "${PROJECT_DIR}/vars.sh"
+
+
+CURRENT_DATE_TIME="`date "+%H_%M_%S"`";
+
+## Create naming 
+FILENAME="${CURRENT_DATE_TIME}.txt"
+
+function logger(){
+    echo -e $1  >> ${LOG_FILE}
+}
+
+function createMedia {
+    logger "\n++ ${CURRENT_DATE_TIME} ++"
+    logger "Creating ${FILENAME}"
+
+    echo 'SOMETHING' >> "${LOCAL_OUTPUT_DIR}/${FILENAME}" 
+}
+
+function uploadMedia {
+    logger "Attempting to upload ${FILENAME} to ${REMOTE_USERNAME}@${REMOTE_ADDRESS}:${REMOTE_OUTPUT_DIR}/${FILENAME}"
+    # Attempt to upload file
+    scp -i "${IDENTITY_LOCATION}" "${LOCAL_OUTPUT_DIR}/${FILENAME}" ${REMOTE_USERNAME}@${REMOTE_ADDRESS}:"${REMOTE_OUTPUT_DIR}/${FILENAME}"
+    
+    # Get status
+    UPLOAD_STATUS=$?
+
+    # If successful remove file
+    if [ ${UPLOAD_STATUS} = 0 ]; then    
+        logger "Successfully uploaded ${FILENAME}"
+        rm  ${LOCAL_OUTPUT_DIR}/${FILENAME}
+
+    else 
+        # Else keep trying
+        logger "Failed to upload ${UPLOAD_STATUS}"
+        #TODO :REACT TO FAILURE
+        # attempt 3 times, if all unsuccessful send mail alert
+    fi
+}
+
+
+# Check directory exists
+if [ ! -d ${LOCAL_OUTPUT_DIR} ]; then
+    logger "* Creating output dir => ${LOCAL_OUTPUT_DIR}" 
+    mkdir -p ${LOCAL_OUTPUT_DIR}
+fi
+
+function main {
+    # Create file if it doesn't already exist
+    # Check log of all files created
+    if [ ! -f ${LOCAL_OUTPUT_DIR}/${FILENAME} ]; then
+        createMedia
+        uploadMedia
+    else
+        uploadMedia
+    fi
+}
+
+main
---
+++ vars-example.sh
@@ -0,0 +1,10 @@
+# Local information
+export LOG_FILE=${PROJECT_DIR}/run_log
+export LOCAL_OUTPUT_DIR=${PROJECT_DIR}/output
+export IDENTITY_LOCATION="location of id_rsa file"
+
+
+# Information of hosting server
+export REMOTE_OUTPUT_DIR="OUTPUT DIR ON SERVER"
+export REMOTE_USERNAME="SERVER USERNAME"
+export REMOTE_ADDRESS="SERVER ADDRESS"
