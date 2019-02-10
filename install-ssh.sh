09bceecb3641affceec578633142121079dfc133
Alberto Lerdo <alb@intentionet.com>
2019-02-09 23:29:51 -0800

    updates

--- install-ssh.sh
+++ install-ssh.sh
@@ -1,25 +1,52 @@
-# install cron that calls the run script 
-
-
-# Get current path
 export PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Get variables
 [ -n "$PROJECT_DIR" ]
 . "${PROJECT_DIR}/vars.sh"
 
-printf "******************************\n"
+function installCronTab {
+    case ${CAPTURE_FREQ} in
+        "1MIN")      
+        echo "1 minute interval selected"
+        (crontab -l ; echo "*/1 * * * * bash $PROJECT_DIR/run.sh") | crontab -
+        ;;
+        "1HR")      
+        echo "1 hour interval selected"
+        ;;
+        "1DAY")      
+        echo "1 day interval selected"
+        ;;
+        *)
+        echo "NO INTERVAL SELECTED"
+        ;;
+    esac
+    crontab -l
+}
+
+
+## Check for requirements when on the pi
+if [[ $(uname) == *"Linux"* ]]; then
+    printf "******************************\n"
+    printf "* Checking for requirements"
+    printf "\n******************************\n"
+    # install cron that calls the run script 
+    dpkg -s fswebcam &> /dev/null
+    if [ $? -eq 0 ]; then
+        echo "fswebcam already installed"
+    else
+        echo "Installing fswebcam ..."
+        sudo apt-get install fswebcam
+    fi
+fi
+
+printf "\n******************************\n"
 printf "* Installing CRON job"
 printf "\n******************************\n"
 
-# Check to see if CRON job is already installed
-if [[ $(crontab -l | egrep -v "^(#|$)" | grep -q 'run'; echo $?) == 1 ]]; then
+if [[ $(crontab -l | grep -q 'run'; echo $?) == 1 ]]; then
     echo "No existing CRON job found, creating one"
-    (crontab -l ; echo "*/1 * * * * bash $PROJECT_DIR/run.sh") | crontab -
-    crontab -l
+    installCronTab;
 else
     printf "* CRON job already exists"
-    printf "\n \n "
     crontab -l
 fi
-
